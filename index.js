f5b78944d4ef3e1759a77812219859b4d9b626f2
gilmar.pereira <gilmar.pereira@itbam.org.br>
2020-08-26 17:10:39 -0400

    Check apk before sending

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ app.use(fileUpload())
 const port = 8852
 const adbLocation = "adb"
 const configs = {
-    targets: ["172.100.11.171", "172.100.11.246", "172.100.11.84"]
+    targets: ["172.100.11.246", "172.100.11.84", "172.100.11.171", "172.100.11.99", "172.100.11.199"]
 }
 
 const { exec } = require('child_process');
 
-exec(`${adbLocation} devices`, (err, stdout, stderr) => {
+
+/* for (const disp of configs.targets) {
+    exec(`${adbLocation} connect ${disp}`, (err, stdout, stderr) => {
+        if (err) {
+            console.log("Comando nao executado.", err);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+} */
+
+
+/* exec(`${adbLocation} devices`, (err, stdout, stderr) => {
     if (err) {
         console.log("Comando nao executado.", err);
         return;
@@ -19,7 +31,7 @@ exec(`${adbLocation} devices`, (err, stdout, stderr) => {
     for (const line of stdout.split("\n")) {
         if (line.length) console.log(line);
     }
-});
+}); */
 
 app.get('/configs', (req, res) => {
     res.send(JSON.stringify(configs));
@@ -38,12 +50,12 @@ app.post('/upload/:target', function (req, res) {
         if (err) return res.status(500).send(err);
         exec(`${adbLocation} -s ${req.params.target} install -r app.apk`, (err, stdout, stderr) => {
             if (err) {
-                console.log("Comando nao executado.", err);
-                res.send('File error!');
+                console.log(err);
+                res.send('Error on install apk!');
                 return;
             }
             console.log("Apk installed.");
-            res.send('File uploaded!');
+            res.send('Apk installed.');
         });
     });
 })
